9b858e2493018a29b4e9c471d995b61b0a1d390f
Arsen Shulika <arsenshulika@gmail.com>
2025-01-26 20:53:56 +0100

    trening

--- js/lesson2.js
+++ js/lesson2.js
@@ -1,4 +1,4 @@
-const arr = [7, 5, "6", 18];
+// const arr = [7, 5, "6", 18];
 // const arr1 = ["3,5,6", 6];
 // console.log(arr.push(8, 9, 10));
 // console.log(arr);
@@ -109,15 +109,116 @@ const arr = [7, 5, "6", 18];
 // console.log(arr2); // [10, 15, 20, 30]
 
 // =======================================
-// const newArr = [...arr];
 
-// function replaceElement(arr, index, newValue, secondValue) {
-//   const result = newArr.splice(index, 1, newValue, secondValue);
-//   console.log(newArr);
+// function replaceElement(index, newValue, secondValue) {
+//   const arr = [7, 5, "6", 18];
+//   const newArr = [...arr];
+//   const result = newArr.splice(index, 2, newValue, secondValue);
 //   console.log(arr);
-//   return newArr;
+//   console.log(newArr);
+
+//   return result;
 // }
 
-// console.log(replaceElement(arr, 2, "ars", "en"));
+// console.log(replaceElement(2, "ars", "en"));
 
 // =========================================
+
+// const buttons = document.querySelectorAll(".btn");
+
+// const handelClick = (event) => console.log(event.target.textContent);
+
+// buttons.forEach((button) => button.addEventListener("click", handelClick));
+
+// =====================================
+
+// function createAdderss(type) {
+//   debugger;
+//   const address = type.toUpperCase();
+//   return function (name) {
+//     return `${address} ${name}`;
+//   };
+// }
+
+// const addressUserOne = createAdderss("Гражданин");
+// const addressUserTwo = createAdderss("Гражданика");
+
+// console.log(addressUserOne("вася"));
+// console.log(addressUserOne("оля"));
+
+// ======================================
+
+// function createPlayer(name) {
+//   debugger;
+//   let score = 0;
+//   return function scoreCount() {
+//     score += 1;
+//     return `${name} - ${score} балов`;
+//   };
+// }
+
+// const playerOne = createPlayer("ars");
+// const playerTwo = createPlayer("ola");
+
+// console.log(playerOne());
+// console.log(playerTwo());
+
+const people = [
+  { name: "Станислав", year: 2000 },
+  { name: "Василий", year: 1986 },
+  { name: "Александр", year: 1990 },
+  { name: "Владимир", year: 2008 },
+];
+
+const comments = [
+  { text: "Отличный пост!", id: 523423 },
+  { text: "Мне нравится", id: 823423 },
+  { text: "Продолжай в том же духе", id: 2039842 },
+  { text: "Все аццццтой !!!!!", id: 123523 },
+  { text: "Нормально", id: 542328 },
+];
+
+//## Методы Some и Every
+
+// Array.prototype.some() -> Проверить, что по крайней мере одному из людей исполнилось 16 лет.
+// !const curDate = new Date();
+// !const curYear = curDate.getFullYear();
+
+// !const checkYear = people.some((person) => curYear - person.year > 38);
+
+// !console.log(curYear);
+
+// Array.prototype.every() -> Проверить, что каждому из людей исполнилось 16 лет.
+
+// !const checkYear = people.every(
+// !  (person) => new Date().getFullYear() - person.year > 16
+// !);
+
+// !console.log(checkYear);
+
+// ## Array.prototype.find() -> Возвращает первый элемент массива, который удовлетворяет нашим требованиям
+
+// !console.log(comments.find((comment) => comment.id === 823423));
+
+// Задача: найти комментарий, c номером ID - 823423
+
+// ## Array.prototype.findIndex() -> Возвращает индекс первого элемента, который удовлетворяет нашим требованиям
+// !const indexOne = comments.findIndex((comment) => comment.id === 123523);
+// !console.log(indexOne);
+
+// Задача: удалить из массива комментарий с номером ID 123523
+// 1. Находим индекс нужного нам комментария
+// !const index = comments.findIndex((comment) => comment.id === 2039842);
+// !console.log(index);
+// 2. Удаляем комментарий из массива (используем метод splice())
+// !const deleteItem = comments.splice(index, 1);
+// !console.log(deleteItem);
+// !console.log(comments);
+// 2.1 Удаляем комментарий из массива (используем метод slice() и оператор spread)
+
+// slice() создает новый массив, куда копирует елементы начиная с индекса старт и до end (не включая end)
+// !const newArr = comments.slice(0, index);
+// !console.log(newArr);
+// используем оператор spread для копирования всех элементов первого и воторого массива в новый массив newComments:
+// !const newComments = [...comments, ...newArr];
+// !console.log(newComments);
---
+++ js/lesson3.js
@@ -0,0 +1,141 @@
+// Создать div
+const divContainer = document.createElement("div");
+
+// Добавить к нему класс wrapper
+divContainer.classList.add("wrapper");
+// Поместить его внутрь тэга body
+const tagBody = document.querySelector("body");
+tagBody.prepend(divContainer);
+
+// Создать заголовок H1 "DOM (Document Object Model)"
+const title = document.createElement("h1");
+title.textContent = "DOM (Document Object Model)";
+
+// Добавить H1 перед DIV с классом wrapper
+tagBody.prepend(title);
+
+// Создать список <ul></ul>
+const unorList = document.createElement("ul");
+// Добавить в него 3 элемента с текстом "один, два, три"
+const item1 = document.createElement("li");
+const item2 = document.createElement("li");
+const item3 = document.createElement("li");
+
+item1.textContent = "один";
+item2.textContent = "два";
+item3.textContent = "три";
+
+unorList.append(item1, item2, item3);
+
+// Поместить список внутрь элемента с классом wrapper
+
+divContainer.append(unorList);
+
+// Создать изображение
+
+const img = document.createElement("img");
+
+// Добавить следующие свойства к изображению
+// 1. Добавить атрибут source
+
+img.setAttribute("src", "https://picsum.photos/240");
+
+// 2. Добавить атрибут width со значением 240
+img.setAttribute("width", "240");
+
+// 3. Добавить класс super
+
+img.classList.add("super");
+
+// 4. Добавить свойство alt со значением "Super Man"
+
+img.alt = "Super Man";
+
+// Поместить изображение внутрь элемента с классом wrapper
+divContainer.append(img);
+
+// Используя HTML строку, создать DIV с классом 'pDiv' + c 2-мя параграфами
+
+const listDiv = `<div class="pDiv">
+  <p></p>
+  <p></p>
+</div>`;
+
+// Поместить этот DIV до элемента <ul></ul>
+unorList.insertAdjacentHTML("beforeend", listDiv);
+
+// Добавить для 2-го параграфа класс text
+const lastText = document.querySelector("p:last-of-type");
+
+lastText.classList.add("text");
+// Удалить 1-й параграф
+
+const firstText = document.querySelector("p:first-of-type");
+
+firstText.remove();
+
+// Создать функцию generateAutoCard,
+// которая принимает 3 аргумента: brand, color, year
+
+const generateAutoCard = (brand, year, color) => {
+  const curDate = new Date();
+  const curYear = curDate.getFullYear();
+  return `
+        <div class="autoCard">
+            <h2>${brand.toUpperCase()} ${year}</h2>
+            <p>Автомобиль ${brand.toUpperCase()} - ${year} года. Возраст авто - ${
+    curYear - year
+  } лет.</p>
+            <p>Цвет: ${color}</p>
+            <button>Удалить</button>
+        </div>
+    `;
+};
+
+// Функция должна возвращать разметку HTML:
+// <div class="autoCard">
+//   <h2>BRAND YEAR</h2>
+//   <p>Автомобиль BRAND - YEAR года. Возраст авто - YEARS лет.</p>
+// </div>
+
+// Создать новый DIV с классом autos
+
+const newDiv = document.createElement("div");
+newDiv.classList.add("autos");
+
+// Создать 3 карточки авто, используя функцию generateAutoCard
+const carsList = [
+  { brand: "Tesla", year: 2015, color: "Красный" },
+  { brand: "Lexus", year: 2016, color: "Серебристый" },
+  { brand: "Nissan", year: 2012, color: "Черный" },
+];
+
+const markUp = carsList
+  .map((car) => {
+    return generateAutoCard(car.brand, car.year, car.color);
+  })
+  .join("");
+
+// Поместить эти 3 карточки внутрь DIV с классом autos
+
+newDiv.insertAdjacentHTML("beforeend", markUp);
+
+// Поместить DIV c классом autos на страницу DOM - до DIV с классом wrapper
+
+divContainer.append(newDiv);
+
+// Добавить кнопку Удалить на каждую карточку авто
+
+// При клике на кнопку - удалять карточку из структуры DOM
+// 1. Выбрать все кнопки
+// 2. Создать функцию удаления
+// 3. Использовать цикл - чтобы повесить обработчик события на каждую кнопку
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => button.addEventListener("click", handleClick));
+
+function handleClick(e) {
+  const currentButton = e.currentTarget;
+  currentButton.closest(".autoCard").remove();
+}
