2c686d8b8cbe9dacce979fe4c00d228805942269
Arsen Shulika <arsenshulika@gmail.com>
2025-01-30 09:01:19 +0100

    tasks

--- js/lesson2.js
+++ js/lesson2.js
@@ -1,13 +1,156 @@
-// const arr = [7, 5, "6", 18];
-// const arr1 = ["3,5,6", 6];
-// console.log(arr.push(8, 9, 10));
-// console.log(arr);
+/*
+? У нас є масив співробітників, відсортуйте його, щоб співробітники не повторювалися
+*/
 
-// function addLastArrValue(arr) {
-//   const arrLength = arr.push(8, 9, 10);
-//   console.log(arr);
-//   return arrLength;
+// const employees = ["Dennis", "Dennis", "Shaw", "Watkins", "Watkins", "Ray"];
+// const filteredEmployees = [];
+
+// for (let i = 0; i < employees.length; i += 1) {
+//   if (employees.indexOf(employees[i]) === i) {
+//     filteredEmployees.push(employees[i]);
+//   }
 // }
+// console.log(filteredEmployees);
+
+// const employees = ["Dennis", "Dennis", "Shaw", "Watkins", "Watkins", "Ray"];
+// const filteredEmployees = [];
+
+// for (let i = 0; i < employees.length; i += 1) {
+//   if (employees.indexOf(employees[i]) === i) {
+//     filteredEmployees.push(employees[i]);
+//   }
+// }
+
+// filteredEmployees.splice(filteredEmployees.indexOf("Shaw"), 1);
+// filteredEmployees.splice(0, 1, "Thorton");
+
+// console.log(filteredEmployees);
+
+/*Потрібно перевітири чи є речення палінром*/
+
+// const str = "Я несу гусеня!";
+// янесугусеня
+// const str = 'Де помити мопед';
+// const str = 'І була пані на палубІ';
+// const str = 'Поліна неси Ром!';
+
+// const updateStr = str.replaceAll(" ", "").toLowerCase();
+// const updateStr2 = [...updateStr].reverse().join("");
+
+// console.log(updateStr === updateStr2);
+
+// const genres = ["Jazz", "Blues", "rock'n'roll", "Country", "Reggy"];
+
+// let longestWord = genres[0];
+
+// for (let i = 0; i < genres.length; i += 1) {
+//   //   console.log(genres[i]);
+//   if (longestWord.length < genres[i].length) {
+//     longestWord = genres[i];
+//   }
+// }
+// console.log(longestWord);
+
+function addNumbers(a, b) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+  return "someone not number";
+}
+
+console.log(addNumbers("t", 3));
+
+/*
+? Напиши функцію min(a,b), яка повертає найменше з чисел a та b.
+? Добав перевірку, що параметри це числа
+*/
+
+function min(a, b) {
+  return typeof a === "number" && typeof b === "number"
+    ? Math.min(a, b)
+    : "not number";
+}
+
+console.log(min(5, 3, 9));
+
+// const str = "JavaScript це цікаво!";
+// let newStr = "";
+// first method:
+
+// for (let i = str.length - 1; i >= 0; i -= 1) {
+//   newStr += str[i];
+// }
+
+// console.log(newStr);
+
+// newStr = str.split("").reverse().join("");
+// console.log(newStr);
+
+// for (let i = 0; i < str.length; i += 1) {
+//   newStr += str[str.length - 1 - i];
+// }
+
+// console.log(newStr);
+
+// const arr = [];
+
+// for (let i = 0; i < str.length; i += 1) {
+//   arr.unshift(str[i]);
+// }
+
+// console.log(arr.join(""));
+
+// const salaries = [200, 450, 600, 150, 300];
+// let total = 0;
+
+// for (let i = 0; i < salaries.length; i += 1) {
+//   total += salaries[i];
+// }
+// console.log(total);
+
+// const managerSalaries = [1000, 1500, 2500, 4000, 5000];
+// const developersSalaries = [8000, 40000, 15000];
+
+// const newArr = [...managerSalaries, ...developersSalaries];
+
+// let total = 0;
+
+// for (let i = 0; i < newArr.length; i += 1) {
+//   total += newArr[i];
+// }
+// console.log(total);
+
+// let total = 0;
+
+// for (let i = 0; i < managerSalaries.length; i += 1) {
+//   total += managerSalaries[i];
+//   if (developersSalaries[i]) {
+//     total += developersSalaries[i];
+//   }
+// }
+
+// console.log(total);
+
+// let newStr = "";
+// const str = "test";
+
+// for (let i = str.length - 1; i >= 0; i -= 1) {
+//   console.log(i);
+//   newStr += str[i];
+// }
+
+// let newStr = "";
+// const str = "abcd";
+
+// for (let i = 0; i < str.length / 2; i += 1) {
+//   newStr = str[i] + newStr;
+//   console.log(str[str.length - 1 - i]);
+// }
+
+// console.log(newStr);
+
+// / const arr = [7, 5, "6", 18];
+// const arr1 = ["3,5,6", 6];
 
 // console.log(addLastArrValue(arr));
 
@@ -163,20 +306,20 @@
 // console.log(playerOne());
 // console.log(playerTwo());
 
-const people = [
-  { name: "Станислав", year: 2000 },
-  { name: "Василий", year: 1986 },
-  { name: "Александр", year: 1990 },
-  { name: "Владимир", year: 2008 },
-];
+// const people = [
+//   { name: "Станислав", year: 2000 },
+//   { name: "Василий", year: 1986 },
+//   { name: "Александр", year: 1990 },
+//   { name: "Владимир", year: 2008 },
+// ];
 
-const comments = [
-  { text: "Отличный пост!", id: 523423 },
-  { text: "Мне нравится", id: 823423 },
-  { text: "Продолжай в том же духе", id: 2039842 },
-  { text: "Все аццццтой !!!!!", id: 123523 },
-  { text: "Нормально", id: 542328 },
-];
+// const comments = [
+//   { text: "Отличный пост!", id: 523423 },
+//   { text: "Мне нравится", id: 823423 },
+//   { text: "Продолжай в том же духе", id: 2039842 },
+//   { text: "Все аццццтой !!!!!", id: 123523 },
+//   { text: "Нормально", id: 542328 },
+// ];
 
 //## Методы Some и Every
 
